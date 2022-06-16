cfa82e567baf8abf0489e333afdfd2bf1ea87ba0
Opilane TTHK <opilane@tthk.ee>
2022-06-16 16:34:20 +0300

    First commit / Add AutoVeodController & HomeController

---
+++ AutoVeod-MVC-Toman/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace AutoVeod_MVC_Toman.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Autoveod-MVC-Toman.Models.Autovedu> Autoveod { get; set; }
+    }
+}
